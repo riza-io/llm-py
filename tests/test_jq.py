f66d9aadfb816aadf489e7313d16c19e66b9e23e
Simon Willison <swillison@gmail.com>
2024-10-26 20:52:54 -0700

    Added tests, closes #3

--- tests/test_jq.py
+++ tests/test_jq.py
@@ -1,6 +1,50 @@
-from llm.plugins import pm
+from click.testing import CliRunner
 
 
-def test_plugin_is_installed():
-    names = [mod.__name__ for mod in pm.get_plugins()]
-    assert "llm_jq" in names
+def test_plugin():
+    import llm
+    from llm.plugins import pm
+
+    class MockModel(llm.Model):
+        model_id = "demo"
+
+        def __init__(self, response_text=""):
+            self.response_text = response_text
+            self.last_prompt = None
+
+        def execute(self, prompt, stream, response, conversation):
+            self.last_prompt = prompt
+            return [self.response_text]
+
+    mock_model = MockModel()
+
+    class TestPlugin:
+        __name__ = "TestPlugin"
+
+        @llm.hookimpl
+        def register_models(self, register):
+            register(mock_model)
+
+    pm.register(TestPlugin(), name="undo")
+    try:
+        from llm.cli import cli
+
+        runner = CliRunner(mix_stderr=False)
+        mock_model.response_text = ".people[0].name"
+        result = runner.invoke(
+            cli,
+            ["jq", "do something", "-m", "demo"],
+            input='{"people": [{"name": "Alice"}]}',
+        )
+        assert result.exit_code == 0
+        assert result.output == '"Alice"\n'
+        assert result.stderr == ".people[0].name\n"
+    finally:
+        pm.unregister(name="undo")
+
+    # Check prompt and response
+    assert mock_model.last_prompt.prompt == (
+        "do something\n\n"
+        + "Example JSON snippet:\n"
+        + '{"people": [{"name": "Alice"}]}'
+    )
